ecc3b395bb7fab3bd0131ba9801c3cd0bf61a80a
Travis Ormsby <travis.ormsby@gmail.com>
2022-02-12 21:29:12 -0600

    working guesser

--- wordlebot.py
+++ wordlebot.py
@@ -1,37 +1,103 @@
+import math
+
 # model pattern as a 5-tuple. Incorrect letter is -1, correct letter is 0, correct letter and position is 1
 
 # For each possible guess, check against each possible answer
 
-from wsgiref.simple_server import software_version
-
-
-guessesFile = "testguesses.txt"
-answersFile = "testanswers.txt"
+guessesFile = "guesses.txt"
+answersFile = "answers.txt"
 
 with open(guessesFile, "r") as g:
     guesses = [word.rstrip() for word in g.readlines()]
 with open(answersFile, "r") as a:
     answers = [word.rstrip() for word in a.readlines()]
-for guess in guesses:
-    for answer in answers:
-        # find all exact matches, remove them, iterate through what's left to find soft matches
-        pattern = {}
-        softMatchAnswer = [l for l in answer]
-        softMatchGuess = [l for l in guess]
-        for index, letter in enumerate(guess):
-            if letter == answer[index]:
-                pattern[index] = 1
-            elif letter in answer:
-                pattern[index] = 0
+
+
+def createPattern(guess, answer):
+    patternDict = {}
+    softMatchGuess = {}
+    softMatchAnswer = [l for l in answer]
+    # find all exact matches, remove them from the softmatch possibilities
+    for index, letter in enumerate(guess):
+        if letter == answer[index]:
+            patternDict[index] = "M"
+            softMatchAnswer.remove(letter)
+        # If it is not an exact match, add it to a dictionary of softmatch possibilities. Use letter position as key to preserve position
+        else:
+            softMatchGuess[index] = letter
+    # Loop through the letters in the guess that aren't exact matches, see if they softmatch any letters in answer that aren't exact matches. Remove them from the softmatch possibilities if they softmatch
+    for index, letter in softMatchGuess.items():
+        if letter in softMatchAnswer:
+            patternDict[index] = "S"
+            softMatchAnswer.remove(letter)
+        # Note if not a softmatch
+        else:
+            patternDict[index] = "X"
+    pattern = "".join([patternDict[k] for k in sorted(patternDict.keys())])
+    return pattern
+
+
+def best_guesser(guesses, answers, answerPattern=None):
+    best_expected_information = 0
+    for guess in guesses:
+        guessDict = {}
+
+        for answer in answers:
+            pattern = createPattern(guess, answer)
+            if pattern in guessDict:
+                guessDict[pattern].append(answer)
             else:
-                pattern[index] = -1
-        print(guess, answer, pattern)
-        # break
-    # break
+                guessDict[pattern] = [answer]
 
+        # expected information is sum of probability of a pattern times information received from that pattern.
+        # information is log2(1/(p(x)))
+        guessDictP = {}
+        totalWords = len(answers)
+        for pattern in guessDict:
+            numWordsMatchingPattern = len(guessDict[pattern])
+            p_of_pattern = numWordsMatchingPattern / totalWords
+            information_of_pattern = math.log(1 / p_of_pattern, 2)
+            guessDictP[pattern] = p_of_pattern * information_of_pattern
+        expected_information = sum(guessDictP.values())
+        # Pick the guess with the highest entropy
+        if expected_information > best_expected_information:
+            best_expected_information = expected_information
+            best_guess = guess
+            best_guessDict = guessDict
+    if answerPattern:
+        return best_guessDict[answerPattern]
+    else:
+        return best_guess
 
-# Create a dictionary where the keys are all possible patterns, values are a 2-tuple of 1) a tuple of all possible solutions and 2) a tuple of all possible guesses with that pattern for a given guess
-
-# Pick the guess with the highest entropy
 
 # Rerun again, with the reduced list of guesses and answers
+def guesser(
+    guessList, guess="soare", answers=None, interactive=False, answer=None, counter=0
+):
+    counter += 1
+    if interactive:
+        guess = input(f"I sugggest you play {guess}. What word did you play? ")
+        pattern = input("What pattern did you get? ")
+    else:
+        pattern = createPattern(guess, answer)
+        print(f"Guessing {guess}. Pattern: {pattern}")
+    if pattern == "MMMMM":
+        print(f"Congratulations! You got it in {counter} guesses!")
+        return
+    else:
+        answers = best_guesser([guess], answers, pattern)
+        if len(answers) == 1:
+            guesser(guessList, answers[0], answers, interactive, answer, counter)
+        else:
+            guess = best_guesser(guessList, answers)
+            guesser(guessList, guess, answers, interactive, answer, counter)
+    return counter
+
+
+testAnswers = ["break", "breed", "briar", "bribe", "brick", "bride", "brief", "brine"]
+
+for answer in testAnswers:
+    guesser(guesses, answers=answers, answer=answer)
+
+
+# guesser(guesses, answers=answers, interactive=True)
